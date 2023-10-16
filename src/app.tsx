9b7d2f273a3bda34bc6ee8fbe7a1f319d0fbb314
Anders Bech Mellson <mellson@icloud.com>
2023-10-16 12:55:08 +0200

    add link to docs

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,19 @@
 export default function App() {
   return (
-    <div className="App">
+    <div className="app">
       <p>
         <span className="sky-header">Stately Sky Starter ⛅</span>
       </p>
-      <a href="https://www.google.com">google</a>
-      <button>hej</button>
+      This is a starter app for Stately Sky ⛅
+      <br />
+      Check out the{' '}
+      <a
+        href="https://stately.ai/docs/stately-sky-getting-started"
+        target="_blank"
+      >
+        docs
+      </a>{' '}
+      for information on how to get started.
     </div>
   );
 }
