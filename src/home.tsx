bb194cb70acae1f8d3710c79e739bd13a98d1bd5
Anders Bech Mellson <mellson@icloud.com>
2023-11-09 10:20:23 +0100

    Styling updates

--- src/home.tsx
+++ src/home.tsx
@@ -1,31 +1,28 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Stately Sky Starter App</h1>
-      <div className="welcome-text">
-        <p>Welcome to the Stately Sky Starter App!</p>
-        This example application is your gateway to exploring state machines in
-        the cloud. Here, you'll find some examples to get you started: click the
-        buttons above to try the examples.{' '}
-        <p>
-          The examples are designed to demonstrate the power and simplicity of
-          managing state with Stately Sky.
-        </p>
-        <p>
-          Dive in and see how easy it is to deploy and interact with state
-          machines using XState V5 in a cloud environment. Happy coding!
-        </p>
-        <p>
-          Check out the{' '}
-          <a
-            href="https://stately.ai/docs/stately-sky-getting-started"
-            target="_blank"
-          >
-            docs
-          </a>{' '}
-          for information on how to get started.
-        </p>
-      </div>
+    <div className="welcome-text">
+      <p>Welcome to the Stately Sky Starter App!</p>
+      This example application is your gateway to exploring state machines in
+      the cloud. Here, you'll find some examples to get you started: click the
+      buttons above to try the examples.{' '}
+      <p>
+        The examples are designed to demonstrate the power and simplicity of
+        managing state with Stately Sky.
+      </p>
+      <p>
+        Dive in and see how easy it is to deploy and interact with state
+        machines using XState V5 in a cloud environment. Happy coding!
+      </p>
+      <p>
+        Check out the{' '}
+        <a
+          href="https://stately.ai/docs/stately-sky-getting-started"
+          target="_blank"
+        >
+          docs
+        </a>{' '}
+        for information on how to get started.
+      </p>
     </div>
   );
 }
